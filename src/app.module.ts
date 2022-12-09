c33dfa7a7ca77cf72e0ae848a057257a0ed26995
LabuPaim <victorpaim80@gmail.com>
2022-12-09 00:58:39 -0300

    nest g resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { DatabaseModule } from './prisma/database.module';
 import { UserRepository } from './users/user.repository';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { CandidatoModule } from './candidato/candidato.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CandidatoModule],
   controllers: [UsersController],
   providers: [UsersService, UserRepository],
 })
