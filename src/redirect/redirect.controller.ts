0aed97175de6b8b291cf19638769f8b2da232c16
LabuPaim <victorpaim80@gmail.com>
2022-12-12 23:05:37 -0300

    tag swagger redirect

--- src/redirect/redirect.controller.ts
+++ src/redirect/redirect.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
 import { Res } from '@nestjs/common/decorators/http/route-params.decorator';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('Redirect')
 export class RedirectController {
   @Get()
   redirect(@Res() res) {
