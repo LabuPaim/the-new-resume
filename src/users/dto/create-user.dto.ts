0f764ddb71d414cddeb672180bdd0334e24bfb82
LabuPaim <victorpaim80@gmail.com>
2022-12-15 11:10:14 -0300

    Desespero com o swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -9,16 +9,11 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty()
   @IsEmail()
+  @ApiProperty()
   email: string;
 
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
   @IsAlphanumeric()
-  @MinLength(8, {
-    message: 'Senha muito curta, mínimo de 8 dígitos',
-  })
+  @ApiProperty()
   password: string;
 }
